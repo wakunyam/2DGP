e38c6f494ec78531f913efa8c0134beb9945d288
LeeYoungJin <dldudwls95@naver.com>
2018-09-18 22:00:54 +0900

    Drill-05 진행8 완성
    에러가 있어서 억지로 고치긴했지만 왜 에러가 생기는지 이해할 수 가 없다
    포인트 3, 4, 5, 9 에서 넘어갈때 y축이 반대로 움직여 -1을 곱하여 에러를 수정하였다

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -12,32 +12,52 @@ character = load_image('animation_sheet.png')
 point = [(203, 535), (132, 243), (535, 470), (477, 203), (715, 136), (316, 225),
          (510, 92), (692, 518), (682, 336), (712, 349)]
 
-move_distance = 7
+move_distance = 3
 
 
 def move_point():
     frame = 0
     x, y = point[0]
+
     for num in range(0, 9 + 1):
-        x1, y1 = point[num]
+        x, y = point[num % 10]
+        x1, y1 = point[num % 10]
         x2, y2 = point[(num + 1) % 10]
-        while x2 - 4 > x or x > x2 + 4:
+        while abs(x2 - x) >= 1 and abs(y2 - y) >= 1:
             if x1 < x2:
                 x += move_distance / math.sqrt(1 + pow((y2 - y1) / (x2 - x1), 2))
+                if y1 < y2:
+                    if num == 3 or num == 4 or num == 5 or num == 9:
+                        y += (y2 - y1) / (x2 - x1) * move_distance / math.sqrt(1 + pow((y2 - y1) / (x2 - x1), 2)) * -1
+                    else:
+                        y += (y2 - y1) / (x2 - x1) * move_distance / math.sqrt(1 + pow((y2 - y1) / (x2 - x1), 2))
+                else:
+                    if num == 3 or num == 4 or num == 5 or num == 9:
+                        y -= (y2 - y1) / (x2 - x1) * move_distance / math.sqrt(1 + pow((y2 - y1) / (x2 - x1), 2)) * -1
+                    else:
+                        y -= (y2 - y1) / (x2 - x1) * move_distance / math.sqrt(1 + pow((y2 - y1) / (x2 - x1), 2))
                 direction = 100
             else:
                 x -= move_distance / math.sqrt(1 + pow((y2 - y1) / (x2 - x1), 2))
+                if y1 < y2:
+                    if num == 3 or num == 4 or num == 5 or num == 9:
+                        y += (y2 - y1) / (x2 - x1) * move_distance / math.sqrt(1 + pow((y2 - y1) / (x2 - x1), 2)) * -1
+                    else:
+                        y += (y2 - y1) / (x2 - x1) * move_distance / math.sqrt(1 + pow((y2 - y1) / (x2 - x1), 2))
+                else:
+                    if num == 3 or num == 4 or num == 5 or num == 9:
+                        y -= (y2 - y1) / (x2 - x1) * move_distance / math.sqrt(1 + pow((y2 - y1) / (x2 - x1), 2)) * -1
+                    else:
+                        y -= (y2 - y1) / (x2 - x1) * move_distance / math.sqrt(1 + pow((y2 - y1) / (x2 - x1), 2))
                 direction = 0
-            if y1 < y2:
-                y += (y2 - y1) / (x2 - x1) * move_distance / math.sqrt(1 + pow((y2 - y1) / (x2 - x1), 2))
-            else:
-                y -= (y2 - y1) / (x2 - x1) * move_distance / math.sqrt(1 + pow((y2 - y1) / (x2 - x1), 2))
+
             clear_canvas()
             grass.draw(400, 30)
             character.clip_draw(frame * 100, direction, 100, 100, x, y)
             update_canvas()
             frame = (frame + 1) % 8
             delay(0.05)
+
 
 
 
