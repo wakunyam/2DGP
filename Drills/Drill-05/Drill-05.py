60f9b3db7dc18d82858200810b429b4a653af252
LeeYoungJin <dldudwls95@naver.com>
2018-09-18 17:49:51 +0900

    코드의 기본구조 완성
    무한 반복하며 사각형과 원을 그리게 된다
    함수의 뼈대를 만들었다

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -4,8 +4,23 @@ open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
-
+'''
+함수를 사용함으로써 코드가 무엇을 하게 될것인지 확실히 해준다
+함수의 머리를 동사로 하여 행동을 정해준다
+'''
 # fill here
+def make_rectangle():
+    pass
+
+
+def make_circle():
+    pass
+
+
+while True:
+    make_rectangle()
+    make_circle()
+
 
     
 close_canvas()
