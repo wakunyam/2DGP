497cc060df27b6d691cfb524990d5d79ea3ede70
LeeYoungJin <dldudwls95@naver.com>
2018-09-18 18:19:59 +0900

    move_rectangle final
    move_rectangle 함수 완성

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -55,11 +55,11 @@ def move_from_left_to_center():
         delay(0.01)
 
 def make_rectangle():
-    #move_from_center_to_right()
-    #move_up()
+    move_from_center_to_right()
+    move_up()
     move_left()
-    #move_down()
-    #move_from_left_to_center()
+    move_down()
+    move_from_left_to_center()
 
 
 def make_circle():
