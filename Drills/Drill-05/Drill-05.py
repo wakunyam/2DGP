d9f06ee54b6cf30c1fb98d98ebf638e06c9f93ba
LeeYoungJin <dldudwls95@naver.com>
2018-09-18 18:26:55 +0900

    진짜 Drill-05 시작
    시작

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -9,76 +9,7 @@ character = load_image('character.png')
 함수의 머리를 동사로 하여 행동을 정해준다
 '''
 # fill here
-def move_from_center_to_right():
-    x, y = 800 // 2, 90
-    while x < 800 - 25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x += 2
-        delay(0.01)
-    
-def move_up():
-    x, y = 800 - 25, 90
-    while y < 600 - 40:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        y += 2
-        delay(0.01)
 
-def move_left():
-    x, y = 800 - 25, 600 - 40
-    while x > 0 + 25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x -= 2
-        delay(0.01)
-
-def move_down():
-    x, y = 0 + 25, 600 - 40
-    while y > 90:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        y -= 2
-        delay(0.01)
-
-def move_from_left_to_center():
-    x, y = 0 + 25, 90
-    while x < 800 // 2:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x += 2
-        delay(0.01)
-
-def make_rectangle():
-    move_from_center_to_right()
-    move_up()
-    move_left()
-    move_down()
-    move_from_left_to_center()
-
-
-def make_circle():
-    cx, cy, r = 800 // 2, 600 // 2, (600 - 180) // 2
-    degree = -90
-    while degree < 270:
-        radian = math.radians(degree)
-        x = cx + r * math.cos(radian)
-        y = cy + r * math.sin(radian)
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        degree += 1
-        delay(0.01)
-
-
-while True:
-    #make_rectangle()
-    make_circle()
 
 
     
