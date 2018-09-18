5f3229684bb5d137755fbbf0f3fcc5b1b7c2df90
LeeYoungJin <dldudwls95@naver.com>
2018-09-18 18:02:30 +0900

    move_rectangle->move_from_center_to_right
    move_rectangle 하위 함수
    move_from_center_to_right 완성

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -10,7 +10,14 @@ character = load_image('character.png')
 '''
 # fill here
 def move_from_center_to_right():
-    pass
+    x, y = 800 // 2, 90
+    while x < 800 - 25:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x += 2
+        delay(0.01)
+    
 def move_up():
     pass
 def move_left():
