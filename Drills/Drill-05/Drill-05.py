ae475269ce39b5fbcda4dc3bb328c952c39d34db
LeeYoungJin <dldudwls95@naver.com>
2018-09-18 18:26:12 +0900

    make_circle final
    make_circle 완성
    math.radians() 로 쉽게 라디안을 구할수있다

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -63,11 +63,21 @@ def make_rectangle():
 
 
 def make_circle():
-    pass
+    cx, cy, r = 800 // 2, 600 // 2, (600 - 180) // 2
+    degree = -90
+    while degree < 270:
+        radian = math.radians(degree)
+        x = cx + r * math.cos(radian)
+        y = cy + r * math.sin(radian)
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        degree += 1
+        delay(0.01)
 
 
 while True:
-    make_rectangle()
+    #make_rectangle()
     make_circle()
 
 
