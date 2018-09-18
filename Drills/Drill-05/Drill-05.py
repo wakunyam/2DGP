0930c892ef527f3546f86ba67ea54c4a344c8133
LeeYoungJin <dldudwls95@naver.com>
2018-09-18 18:59:51 +0900

    리스트 취소
    리스트 사용법을 모르겠음

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -9,12 +9,29 @@ character = load_image('animation_sheet.png')
 함수의 머리를 동사로 하여 행동을 정해준다
 '''
 # fill here
-point = {(203, 535), (132, 243), (535, 470), (477, 203), (715, 136), (316, 225), (510, 92), (692, 518),
-         (682, 336), (712, 349)}
-def move_point():
-  
+point1, point2, point3, point4, point5, point6, point7, point8, point9, point10 = (203, 535), (132, 243), (535, 470), (477, 203), (715, 136), (316, 225), (510, 92), (692, 518), (682, 336), (712, 349)
 
-    pass
+
+def move_point():
+
+
+
+    '''
+    frame = 0
+     while True:
+        clear_canvas()
+        grass.draw(400, 90)
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.05)
+    '''
+
+
+
+
+
+
 
 while True:
     move_point()
