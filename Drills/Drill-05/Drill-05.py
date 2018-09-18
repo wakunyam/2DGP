2fedb04826aa7a9c508ca604bee5f98d441ddbbc
LeeYoungJin <dldudwls95@naver.com>
2018-09-18 19:59:12 +0900

    Drill-05 진행4
    리스트의 사용방법을 알아냄

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -9,20 +9,20 @@ character = load_image('animation_sheet.png')
 함수의 머리를 동사로 하여 행동을 정해준다
 '''
 # fill here
-point1, point2, point3, point4, point5, point6, point7, point8, point9, point10 = (203, 535), (132, 243), (535, 470), (477, 203), (715, 136), (316, 225), (510, 92), (692, 518), (682, 336), (712, 349)
+point = [(203, 535), (132, 243), (535, 470), (477, 203), (715, 136), (316, 225), (510, 92), (692, 518), (682, 336), (712, 349)]
 
 
 def move_point():
     frame = 0
-    x, y = point1
+    num = 0
+    x, y = point[1]
     while 1:
         clear_canvas()
         grass.draw(400, 30)
         character.clip_draw(frame * 100, 100, 100, 100, x, y)
         update_canvas()
         frame = (frame + 1) % 8
-        x -= 3
-        y += (243 - 535 / 132 - 203) * x
+
         delay(0.05)
 
 
