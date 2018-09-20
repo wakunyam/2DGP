f0de401d4a7b9b4ec451c6db1f158b64a39dc58b
LeeYoungJin <dldudwls95@naver.com>
2018-09-20 18:15:10 +0900

    키보드입력
    움직임이 좀더 자연스럽게 수정중

--- Drills/Drill-06/move_character_with_key.py
+++ Drills/Drill-06/move_character_with_key.py
@@ -4,17 +4,28 @@ from pico2d import *
 def handle_events():
     global running
     global x
+    global dir
+    global predir
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                x = x + 10
+                dir += 1
             elif event.key == SDLK_LEFT:
-                x = x - 10
+                dir -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+                predir = 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
+                predir = -1
+
 
 
 open_canvas()
@@ -24,16 +35,21 @@ character = load_image('animation_sheet.png')
 running = True
 x = 800 // 2
 frame = 0
+dir = 0
+predir = 1
 
 while running:
     clear_canvas()
     grass.draw(400, 30)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
+    if predir == 1 or dir == 1:
+        character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+    elif predir == -1 or dir == -1:
+        character.clip_draw(frame * 100, 0, 100, 100, x, 90)
     update_canvas()
 
     handle_events()
     frame = (frame + 1) % 8
-
+    x += dir * 5
     delay(0.05)
 
 close_canvas()
