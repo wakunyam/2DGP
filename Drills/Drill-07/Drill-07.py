3bd0fb79beea9dffbb7851abcef14d60ec2a4dce
LeeYoungJin <dldudwls95@naver.com>
2018-10-02 19:12:08 +0900

    Drill-07 03 완성
    open_canvas에 수치를 잘못 넣어줬었다
    수정 완료

--- Drills/Drill-07/Drill-07.py
+++ Drills/Drill-07/Drill-07.py
@@ -3,7 +3,7 @@ import random
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
-open_canvas(KPU_WIDTH, KPU_WIDTH)
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
 
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
