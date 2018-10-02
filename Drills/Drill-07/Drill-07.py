de385f081ccdc6a43d96406ee6c83e7468247e50
LeeYoungJin <dldudwls95@naver.com>
2018-10-02 19:09:24 +0900

    Drill-07 02
    함수 완성
    캐릭터의 좌표를 화면에 중심에 오도록
    포인트의 리스트를 수정해주었다
    
    사진의 위치가 맞지 않는다

--- Drills/Drill-07/Drill-07.py
+++ Drills/Drill-07/Drill-07.py
@@ -1,21 +1,33 @@
 from pico2d import *
 import random
 
-open_canvas()
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
-grass = load_image('KPU_GROUND.png')
+open_canvas(KPU_WIDTH, KPU_WIDTH)
+
+kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
 size = 20
-points = [(random.randint(0, 1000), random.randint(0, 1000)) for i in range(size)]
+points = [(random.randint(-500 + KPU_WIDTH // 2, 500 + KPU_WIDTH // 2),
+           random.randint(-500 + KPU_HEIGHT // 2, 500 + KPU_HEIGHT // 2)) for i in range(size)]
+
 
 def move_line(p1, p2):
     frame = 0
-
-    for i in range(0, 100 + 1, 5)
+    for i in range(0, 100 + 1, 2):
+        clear_canvas()
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
         t = i / 100
-    
-    pass
+        character_x = (1 - t) * p1[0] + t * p2[0]
+        character_y = (1 - t) * p1[1] + t * p2[1]
+        if p1[0] < p2[0]:
+            character.clip_draw(frame * 100, 100, 100, 100, character_x, character_y)
+        else:
+            character.clip_draw(frame * 100, 0, 100, 100, character_x, character_y)
+        frame = (frame + 1) % 8
+        delay(0.02)
+        update_canvas()
 
 n = 1
 
