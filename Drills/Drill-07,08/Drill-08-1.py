33a04a855adb40470baf3c6f77537adc6ba71744
LeeYoungJin <dldudwls95@naver.com>
2018-10-04 18:40:04 +0900

    2014182031 이영진 Drill-08-1 완성  커밋횟수 : 2회
    https://www.youtube.com/watch?v=6SXbsOBBJcA
    
    4점을 찍는 함수를 이용하여
    모든 곡선을 4점에 영향을 받는 곡선으로 만들어서 완성

--- Drills/Drill-07,08/Drill-08-1.py
+++ Drills/Drill-07,08/Drill-08-1.py
@@ -95,17 +95,20 @@ def draw_curve_4_points(p1, p2, p3, p4):
         draw_point((x, y))
     draw_point(p4)
 
-def draw_curve_5_points(p1, p2, p3, p4, p5):
+
+def draw_curve_4_points(p1, p2, p3, p4):
     draw_big_point(p1)
     draw_big_point(p2)
     draw_big_point(p3)
     draw_big_point(p4)
 
     # draw p1-p2
-    for i in range(0, 50, 2):
+    for i in range(0, 100, 2):
         t = i / 100
-        x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
-        y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p2[0] + (t ** 3 - t ** 2) * p3[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p2[1] + (t ** 3 - t ** 2) * p3[1]) / 2
         draw_point((x, y))
     draw_point(p2)
 
@@ -121,25 +124,27 @@ def draw_curve_5_points(p1, p2, p3, p4, p5):
     for i in range(0, 100, 2):
         t = i / 100
         x = ((-t ** 3 + 2 * t ** 2 - t) * p2[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[0] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p4[0] + (t ** 3 - t ** 2) * p5[0]) / 2
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p4[0] + (t ** 3 - t ** 2) * p1[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p2[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[1] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p5[1]) / 2
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p1[1]) / 2
         draw_point((x, y))
-    draw_point(p3)
+    draw_point(p4)
 
-    # draw p4-p5
-    for i in range(50, 100, 2):
+    for i in range(0, 100, 2):
         t = i / 100
-        x = (2*t**2-3*t+1)*p3[0]+(-4*t**2+4*t)*p4[0]+(2*t**2-t)*p5[0]
-        y = (2*t**2-3*t+1)*p3[1]+(-4*t**2+4*t)*p4[1]+(2*t**2-t)*p5[1]
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p3[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p1[0] + (t ** 3 - t ** 2) * p2[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p1[1] + (t ** 3 - t ** 2) * p2[1]) / 2
         draw_point((x, y))
-    draw_point(p5)
+    draw_point(p1)
 
 
 prepare_turtle_canvas()
 
 points=[(-300,200),(400,350),(300,-300),(-200,-200)]
 
-draw_curve_5_points(points[0],points[1],points[2],points[3],points[0])
+while True:
+    draw_curve_4_points(points[0],points[1],points[2],points[3])
 
 turtle.done()
