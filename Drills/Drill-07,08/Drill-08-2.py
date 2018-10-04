7d4fc80cafcb9d0383bcd1b3a55b4214c9b07c6e
LeeYoungJin <dldudwls95@naver.com>
2018-10-04 19:06:18 +0900

    Drill-08-2 02
    캐릭터가 좌표에 도착하면 캐릭터를 그린다를 수행하자

--- Drills/Drill-07,08/Drill-08-2.py
+++ Drills/Drill-07,08/Drill-08-2.py
@@ -12,42 +12,27 @@ points = [(random.randint(-500 + KPU_WIDTH // 2, 500 + KPU_WIDTH // 2),
            random.randint(-500 + KPU_HEIGHT // 2, 500 + KPU_HEIGHT // 2)) for i in range(size)]
 
 
-def draw_curve_4_points(p1, p2, p3, p4):
-
-    # draw p1-p2
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[0] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p2[0] + (t ** 3 - t ** 2) * p3[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[1] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p2[1] + (t ** 3 - t ** 2) * p3[1]) / 2
-
-
-    # draw p2-p3
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
-        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
-
-
-    # draw p3-p4
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p2[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[0] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p4[0] + (t ** 3 - t ** 2) * p1[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p2[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[1] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p1[1]) / 2
-
-
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p3[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[0] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p1[0] + (t ** 3 - t ** 2) * p2[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p1[1] + (t ** 3 - t ** 2) * p2[1]) / 2
+def draw_curve(p, size):
+    frame = 0
+    for j in range(0, size):
+        for i in range(0, 100, 2):
+            clear_canvas()
+            kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+            t = i / 100
+            character_x = ((-t ** 3 + 2 * t ** 2 - t) * p[j][0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p[(j+1)%(size)][0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p[(j+2)%(size)][0] + (t ** 3 - t ** 2) * p[(j+3)%(size)][0]) / 2
+            character_y = ((-t ** 3 + 2 * t ** 2 - t) * p[j][1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p[(j+1)%(size)][1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p[(j+2)%(size)][1] + (t ** 3 - t ** 2) * p[(j+3)%(size)][1]) / 2
+            if p[j][0] < p[(j+1)%(size)][0]:
+                character.clip_draw(frame * 100, 100, 100, 100, character_x, character_y)
+            else:
+                character.clip_draw(frame * 100, 0, 100, 100, character_x, character_y)
+            frame = (frame + 1) % 8
+            delay(0.02)
+            update_canvas()
 
 
 while True:
-    draw_curve_4_points()
+    draw_curve(points, size)
 
 close_canvas()
