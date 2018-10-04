dc388c26f72e34baf2ee9cb64c24ec4056809a84
LeeYoungJin <dldudwls95@naver.com>
2018-10-04 18:28:40 +0900

    Drill-08-1 01
    5점을 이을수있게 함수를 수정중

--- Drills/Drill-07,08/Drill-08-1.py
+++ Drills/Drill-07,08/Drill-08-1.py
@@ -51,11 +51,18 @@ def draw_point(p):
     turtle.dot(5, random.random(), random.random(), random.random())
 
 
-
-
 def draw_curve_3_points(p1, p2, p3):
-    # fill here
-    pass
+    draw_big_point(p1)
+    draw_big_point(p2)
+    draw_big_point(p3)
+
+    for i in range(50, 100, 2):
+        t = i / 100
+        x = (2*t**2-3*t+1)*p1[0] + (-4*t**2+4*t)*p2[0] + (2*t**2-t)*p3[0]
+        y = (2*t**2-3*t+1)*p1[1] + (-4*t**2+4*t)*p2[1] + (2*t**2-t)*p3[1]
+        draw_point((x, y))
+
+    draw_point(p3)
 
 
 def draw_curve_4_points(p1, p2, p3, p4):
@@ -88,14 +95,51 @@ def draw_curve_4_points(p1, p2, p3, p4):
         draw_point((x, y))
     draw_point(p4)
 
+def draw_curve_5_points(p1, p2, p3, p4, p5):
+    draw_big_point(p1)
+    draw_big_point(p2)
+    draw_big_point(p3)
+    draw_big_point(p4)
 
+    # draw p1-p2
+    for i in range(0, 50, 2):
+        t = i / 100
+        x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
+        y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
+        draw_point((x, y))
+    draw_point(p2)
 
+    # draw p2-p3
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
+        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
+        draw_point((x, y))
+    draw_point(p3)
+
+    # draw p3-p4
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p2[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p4[0] + (t ** 3 - t ** 2) * p5[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p2[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p5[1]) / 2
+        draw_point((x, y))
+    draw_point(p3)
+
+    # draw p4-p5
+    for i in range(50, 100, 2):
+        t = i / 100
+        x = (2*t**2-3*t+1)*p3[0]+(-4*t**2+4*t)*p4[0]+(2*t**2-t)*p5[0]
+        y = (2*t**2-3*t+1)*p3[1]+(-4*t**2+4*t)*p4[1]+(2*t**2-t)*p5[1]
+        draw_point((x, y))
+    draw_point(p5)
 
 
 prepare_turtle_canvas()
 
+points=[(-300,200),(400,350),(300,-300),(-200,-200)]
 
-
-
+draw_curve_5_points(points[0],points[1],points[2],points[3],points[0])
 
 turtle.done()
