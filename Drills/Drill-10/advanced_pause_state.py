a78e44677d2e9c64618d9f7380f4d5e14c1ff009
LeeYoungJin <dldudwls95@naver.com>
2018-10-11 20:36:23 +0900

    Drill-10-2 01
    pause_state 세분화

--- Drills/Drill-10/advanced_pause_state.py
+++ Drills/Drill-10/advanced_pause_state.py
@@ -20,6 +20,8 @@ def handle_events():
     for event in events:
         if (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
             game_framework.pop_state()
+        elif (event.type == SDL_QUIT):
+            game_framework.quit()
 
 
 def draw():
