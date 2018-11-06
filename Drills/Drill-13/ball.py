ad402616f736654547e39ae9c1101ae40186ea40
LeeYoungJin <dldudwls95@naver.com>
2018-11-06 18:17:45 +0900

    lec13

---
+++ Drills/Drill-13/ball.py
@@ -0,0 +1,50 @@
+import random
+from pico2d import *
+import game_world
+import game_framework
+
+class Ball:
+    image = None
+
+    def __init__(self):
+        if Ball.image == None:
+            Ball.image = load_image('ball21x21.png')
+        self.x, self.y, self.fall_speed = random.randint(0, 1600-1), 60, 0
+
+    def get_bb(self):
+        # fill here
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+        # fill here for draw
+        draw_rectangle(*self.get_bb())
+
+    def update(self):
+        self.y -= self.fall_speed * game_framework.frame_time
+
+    #fill here for def stop
+    def stop(self):
+        self.fall_speed = 0
+
+
+# fill here
+# class BigBall
+class BigBall(Ball):
+    MIN_FALL_SPEED = 50
+    MAX_FALL_SPEED = 200
+    image = None
+
+    def __init__(self):
+        if BigBall.image == None:
+            BigBall.image = load_image('ball41x41.png')
+        self.x, self.y = random.randint(0, 1600 - 1), 500
+        self.fall_speed = random.randint(BigBall.MIN_FALL_SPEED, BigBall.MAX_FALL_SPEED)
+
+    def update(self):
+        self.y -= self.fall_speed * game_framework.frame_time
+        if self.y <= 70:
+            self.y = 70
+
+    def get_bb(self):
+        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
--- Drills/Drill-13/game_world.py
+++ Drills/Drill-13/game_world.py
@@ -1,11 +1,16 @@
 
 # layer 0: Background Objects
 # layer 1: Foreground Objects
-objects = [[], []]
+objects = [[],[]]
 
 
 def add_object(o, layer):
     objects[layer].append(o)
+
+
+def add_objects(l, layer):
+    for o in l:
+        add_object(o, layer)
 
 
 def remove_object(o):
@@ -25,3 +30,4 @@ def all_objects():
     for i in range(len(objects)):
         for o in objects[i]:
             yield o
+
